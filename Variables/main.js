ca3c7fa16e473aca033968318c8648971c94bab7
Ivan Savchuk <isavchuk99@gmail.com>
2018-08-22 18:36:58 +0300

    add Variables
    add Variables for my code ;)

---
+++ Variables/main.js
@@ -0,0 +1,14 @@
+var number = 19;
+
+var string;
+string = "Hello World! ";
+
+var floatNumber = 1.25;
+
+var isTrue;
+isTrue = true;
+
+document.write("Variable number = " + number + 
+    "<br/>" + string + floatNumber + 
+    "<br/>" + isTrue
+);
