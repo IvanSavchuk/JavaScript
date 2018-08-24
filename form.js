c05e1a0dcfec3ead428f279e67ddaa4da8521953
Ivan Savchuk <isavchuk99@gmail.com>
2018-08-24 22:38:36 +0300

    add Form processing #2

--- form.js
+++ form.js
@@ -1,13 +1,22 @@
 function valid(form) {
     var fail = false;
     var name = form.name.value;
+    var email = form.email.value;
     var password = form.password.value;
     var RePassword = form.RePassword.value;
     var state = form.state.value;
+
+    var adr_pattern =/[0-9a-z_]+@[0-9a-z_]+\.[a-z]{2,5}/i;
+
     if(name == "" || name == " ") {
-        fail = "Вы не вели свое имя";
+        fail = "Вы не ввели свое имя";
+    } else if(adr_pattern.test(email) == false) {
+        fail = "Вы ввели email неправильно"; 
+        // else if(email.split('@').length -1 == 0) {
+        //     fail = "Вы ввели email неправильно"; 
+        // }   
     } else if(password == "") {
-        fail = "Вы не вели пароль";
+        fail = "Вы не ввели пароль";
     } else if(password != RePassword) {
         fail = "Пароли не совпадают";
     } else if(state == "") {
@@ -17,6 +26,6 @@ function valid(form) {
     if(fail) {
         alert(fail);
     } else {
-        window.location = "https://github.com/IvanSavchuk/JavaScript";
+        window.location = "https://github.com/IvanSavchuk";
     }
 }
