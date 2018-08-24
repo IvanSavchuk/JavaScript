6f99a9eb5d71c5e3fd3cda00841d7961009d489f
Ivan Savchuk <isavchuk99@gmail.com>
2018-08-24 14:15:04 +0300

    add Method prompt

--- methods.js
+++ methods.js
@@ -1,5 +1,6 @@
-var person = "John";
+var person;
 if(confirm("Are you sure?")){
+    person = prompt("Hi, what`s your name?")
     alert("Hello, " + person);
 } else {
     alert("Person not sure");
